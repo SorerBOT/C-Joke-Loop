d96c1e2893119c93f2064042ce6eff590e6c3ac0
SorerBOT <alonfiller@gmail.com>
2022-07-31 11:59:41 +0300

    The actual joke

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv[]) {
+  for (int H = 0; H < 1; H++) {
+      printf("H");
+      fflush( stdout );
+      for (int E = 0; E < 1; E++) {
+          printf("e");
+          fflush( stdout );
+          for (int L = 0; L < 1; L++) {
+              printf("l");
+              fflush( stdout );
+              for (L = 0; L < 1; L++) {
+                  printf("l");
+                  fflush( stdout );
+                  for (int O = 0; O < 1; O++) {
+                      printf("o");
+                      fflush( stdout );
+                      for (int space = 0; space < 1; space++) {
+                          printf(" ");
+                          fflush( stdout );
+                          for (int W = 0; W < 1; W++) {
+                              printf("W");
+                              fflush( stdout );
+                              for (O = 0; O < 1; O++) {
+                                  printf("o");
+                                  fflush( stdout );
+                                  for (int R = 0; R < 1; R++) {
+                                      printf("r");
+                                      fflush( stdout );
+                                      for (L = 0; L < 1; L++) {
+                                          printf("l");
+                                          fflush( stdout );
+                                          for (int D; D < 1; D++) {
+                                              printf("d");
+                                              fflush( stdout );
+                                              for (int exclamationMark = 0; exclamationMark < 1; exclamationMark++) {
+                                                  printf("!");
+                                                  fflush( stdout );
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+  return 0;
+}
